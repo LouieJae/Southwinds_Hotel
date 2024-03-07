8cfe3024950ea7d2f428b3b8204d73860330b86a
charlesclassify <charles@classify.com.ph>
2024-03-07 08:10:07 +0800

    footer

--- application/views/Bookings/footer.php
+++ application/views/Bookings/footer.php
@@ -84,139 +84,140 @@
 <script>
     // Your DataTable initialization scripts
     $(document).ready(function() {
-                $('#user-datatables').dataTable({
-                    "lengthMenu": [5, 10, 25, 50, 75, 100]
-                });
+        $('#user-datatables').dataTable({
+            "lengthMenu": [5, 10, 25, 50, 75, 100]
+        });
 
-                var sidebarOpen = true; // Initial state of the sidebar
-                var toggleBtn = document.querySelector('.toggle-btn');
+        var sidebarOpen = true; // Initial state of the sidebar
+        var toggleBtn = document.querySelector('.toggle-btn');
 
-                function toggleSidebar() {
-                    var sidebar = document.querySelector('.sidebar');
-                    var content = document.querySelector('.content');
+        function toggleSidebar() {
+            var sidebar = document.querySelector('.sidebar');
+            var content = document.querySelector('.content');
 
-                    sidebarOpen = !sidebarOpen; // Toggle the state
+            sidebarOpen = !sidebarOpen; // Toggle the state
 
-                    if (sidebarOpen) {
-                        sidebar.style.left = '0';
-                        content.style.marginLeft = '260px';
-                        toggleBtn.style.marginLeft = '260px';
-                    } else {
-                        sidebar.style.left = '-260px';
-                        content.style.marginLeft = '0';
-                        toggleBtn.style.marginLeft = '15px';
-                    }
+            if (sidebarOpen) {
+                sidebar.style.left = '0';
+                content.style.marginLeft = '260px';
+                toggleBtn.style.marginLeft = '260px';
+            } else {
+                sidebar.style.left = '-260px';
+                content.style.marginLeft = '0';
+                toggleBtn.style.marginLeft = '15px';
+            }
+        }
+
+        $(document).ready(function() {
+            // Toggle dropdown when clicking on it
+            $('.custom-dropdown').on('click', function(event) {
+                $('.custom-dropdown').not($(this)).removeClass('open'); // Close other dropdowns
+                $(this).toggleClass('open');
+                event.stopPropagation(); // Prevent the click event from propagating to the document
+            });
+
+            // Close dropdown when clicking outside of it
+            $(document).on('click', function(event) {
+                if (!$(event.target).closest('.custom-dropdown').length) {
+                    $('.custom-dropdown').removeClass('open');
                 }
+            });
+        });
 
-                $(document).ready(function() {
-                    // Toggle dropdown when clicking on it
-                    $('.custom-dropdown').on('click', function(event) {
-                        $('.custom-dropdown').not($(this)).removeClass('open'); // Close other dropdowns
-                        $(this).toggleClass('open');
-                        event.stopPropagation(); // Prevent the click event from propagating to the document
-                    });
+        // Define a variable to store the state of the sidebar
+        var isSidebarOpen = true;
 
-                    // Close dropdown when clicking outside of it
-                    $(document).on('click', function(event) {
-                        if (!$(event.target).closest('.custom-dropdown').length) {
-                            $('.custom-dropdown').removeClass('open');
-                        }
-                    });
-                });
+        function toggleSidebar() {
+            var sidebar = document.querySelector('.sidebar');
+            var content = document.querySelector('.content');
+            var toggleBtn = document.querySelector('.toggle-btn');
 
-                // Define a variable to store the state of the sidebar
-                var isSidebarOpen = true;
-
-                function toggleSidebar() {
-                    var sidebar = document.querySelector('.sidebar');
-                    var content = document.querySelector('.content');
-                    var toggleBtn = document.querySelector('.toggle-btn');
-
-                    if (window.innerWidth <= 767) {
-                        // Close sidebar if the screen size is smaller than or equal to 767px
-                        sidebar.style.width = '0';
-                        content.style.marginLeft = '0';
-                        toggleBtn.style.marginLeft = '10px'; // Adjust margin for better spacing
-                        isSidebarOpen = false;
-                    } else {
-                        // Toggle sidebar for larger screens
-                        if (isSidebarOpen) {
-                            sidebar.style.width = '0';
-                            content.style.marginLeft = '0';
-                            toggleBtn.style.marginLeft = '10px'; // Adjust margin for better spacing
-                            isSidebarOpen = false;
-                        } else {
-                            sidebar.style.width = '250px';
-                            content.style.marginLeft = '250px';
-                            toggleBtn.style.marginLeft = '260px'; // Adjust margin for better spacing
-                            isSidebarOpen = true;
-                        }
-                    }
-
-                    // Show or hide sidebar items based on the sidebar state
-                    var sidebarLinks = document.querySelectorAll('.sidebar a');
-                    for (var i = 0; i < sidebarLinks.length; i++) {
-                        sidebarLinks[i].style.display = isSidebarOpen ? 'block' : 'none';
-                    }
+            if (window.innerWidth <= 767) {
+                // Close sidebar if the screen size is smaller than or equal to 767px
+                sidebar.style.width = '0';
+                content.style.marginLeft = '0';
+                toggleBtn.style.marginLeft = '10px'; // Adjust margin for better spacing
+                isSidebarOpen = false;
+            } else {
+                // Toggle sidebar for larger screens
+                if (isSidebarOpen) {
+                    sidebar.style.width = '0';
+                    content.style.marginLeft = '0';
+                    toggleBtn.style.marginLeft = '10px'; // Adjust margin for better spacing
+                    isSidebarOpen = false;
+                } else {
+                    sidebar.style.width = '250px';
+                    content.style.marginLeft = '250px';
+                    toggleBtn.style.marginLeft = '260px'; // Adjust margin for better spacing
+                    isSidebarOpen = true;
                 }
+            }
 
-                $(document).ready(function() {
-                    // Function to highlight the active link in the sidebar
-                    function highlightActiveLink() {
-                        var currentUrl = window.location.href;
+            // Show or hide sidebar items based on the sidebar state
+            var sidebarLinks = document.querySelectorAll('.sidebar a');
+            for (var i = 0; i < sidebarLinks.length; i++) {
+                sidebarLinks[i].style.display = isSidebarOpen ? 'block' : 'none';
+            }
+        }
 
-                        // Remove active class from all sidebar links
-                        $('.sidebar a').removeClass('active');
+        $(document).ready(function() {
+            // Function to highlight the active link in the sidebar
+            function highlightActiveLink() {
+                var currentUrl = window.location.href;
 
-                        // Iterate through each sidebar link
-                        $('.sidebar a').each(function() {
-                            // Check if the link should be excluded from highlighting
-                            if (!$(this).hasClass('exclude-from-highlight')) {
-                                var linkUrl = $(this).attr('href');
+                // Remove active class from all sidebar links
+                $('.sidebar a').removeClass('active');
 
-                                // Check if the current URL contains the link URL
-                                if (currentUrl.indexOf(linkUrl) !== -1) {
-                                    // Add the active class to the matching link
-                                    $(this).addClass('active');
-                                }
-                            }
-                        });
+                // Iterate through each sidebar link
+                $('.sidebar a').each(function() {
+                    // Check if the link should be excluded from highlighting
+                    if (!$(this).hasClass('exclude-from-highlight')) {
+                        var linkUrl = $(this).attr('href');
+
+                        // Check if the current URL contains the link URL
+                        if (currentUrl.indexOf(linkUrl) !== -1) {
+                            // Add the active class to the matching link
+                            $(this).addClass('active');
+                        }
                     }
-
-                    // Call the function when the page loads
-                    highlightActiveLink();
                 });
+            }
+
+            // Call the function when the page loads
+            highlightActiveLink();
+        });
 
 
-                // Chart.js code
-                const ctx = document.getElementById('roomChart').getContext('2d');
-                const chartData = {
-                    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-                    datasets: [{
-                        label: 'Sales',
-                        data: [2000, 3000, 2500, 4000, 3500, 5000, 4500], // Sample data, replace with your actual data
-                        fill: false,
-                        borderColor: 'rgba(75, 192, 192, 1)',
-                        backgroundColor: 'rgba(75, 192, 192, 0.2)',
-                        pointBackgroundColor: 'rgba(75, 192, 192, 1)',
-                        pointBorderColor: '#fff',
-                        pointHoverBackgroundColor: '#fff',
-                        pointHoverBorderColor: 'rgba(75, 192, 192, 1)'
+        // Chart.js code
+        const ctx = document.getElementById('roomChart').getContext('2d');
+        const chartData = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [{
+                label: 'Sales',
+                data: [2000, 3000, 2500, 4000, 3500, 5000, 4500], // Sample data, replace with your actual data
+                fill: false,
+                borderColor: 'rgba(75, 192, 192, 1)',
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                pointBackgroundColor: 'rgba(75, 192, 192, 1)',
+                pointBorderColor: '#fff',
+                pointHoverBackgroundColor: '#fff',
+                pointHoverBorderColor: 'rgba(75, 192, 192, 1)'
+            }]
+        };
+
+        const roomChart = new Chart(ctx, {
+            type: 'line',
+            data: chartData,
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
                     }]
-                };
+                }
+            }
 
-                const roomChart = new Chart(ctx, {
-                    type: 'line',
-                    data: chartData,
-                    options: {
-                        scales: {
-                            yAxes: [{
-                                ticks: {
-                                    beginAtZero: true
-                                }
-                            }]
-                        }
-                    }
-
-                });
+        });
+    });
 </script>
